8aafd4f60504283b68d9f3b9a2faec53e83bcd05
AlktaThaci <alketathacii@gmail.com>
2025-02-06 23:25:03 +0100

    Funkisioni per kalkulimin e notaev

---
+++ projekt4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void Kalkulatori() {
+	char perserit;
+
+	cout << "\nKalkulatori-\n";
+	int numriNotave;
+	cout << "Shkruani numrin e notave qe keni: ";
+	cin >> numriNotave;
+
+	if (numriNotave <= 0) {
+		cout << "Numri i notave duhet te jete me i madh se zero\n";
+		return ;
+	}
+}
